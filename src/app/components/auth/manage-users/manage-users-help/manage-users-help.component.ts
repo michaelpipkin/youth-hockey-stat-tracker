6321f6926e22caf45062ef605ce64d9fdce51932
michaelpipkin <michael.a.pipkin@gmail.com>
2024-09-24 18:59:11 -0400

    Add manage users help

--- src/app/components/auth/manage-users/manage-users-help/manage-users-help.component.ts
+++ src/app/components/auth/manage-users/manage-users-help/manage-users-help.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-manage-users-help',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatDialogModule],
   templateUrl: './manage-users-help.component.html',
-  styleUrl: './manage-users-help.component.scss'
+  styleUrl: './manage-users-help.component.scss',
 })
-export class ManageUsersHelpComponent {
-
-}
+export class ManageUsersHelpComponent {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   useEmulators: false,
-  buildDate: new Date('2024-09-24T22:28:04.695Z')
+  buildDate: new Date('2024-09-24T22:58:31.138Z')
 };
